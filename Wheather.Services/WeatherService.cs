665a29421611c359914663975d2a19349793a6e5
Juliano Martins <julianoalm@gmail.com>
2020-09-02 21:56:10 -0300

    Refatorando

--- Wheather.Services/WeatherService.cs
+++ Wheather.Services/WeatherService.cs
@@ -34,7 +34,7 @@ namespace Weather.Services
             if (!string.IsNullOrEmpty(name))
             {
                 var rng = new Random();
-                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                return Enumerable.Range(1, 10).Select(index => new WeatherForecast
                 {
                     Date = DateTime.Now.AddDays(index),
                     TemperatureC = rng.Next(-20, 55),
